602ac32370a39b63607b182c602d78d1ba00e4ef
I K H S A N / <Ikhsancandra721@gmail.com>
2026-09-15 19:41:36 +0700

    main.cpp to greet user by name

---
+++ P1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama;
+    cout<<"Siapa Nama kamu: ";
+    getline(cin, nama);
+    cout<<"Halo "<<nama<<"! Selamat datang";
+
+    return 0;
+}
