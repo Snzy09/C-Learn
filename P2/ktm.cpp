2e007bedd72cbdbe7f77ce5162c4d96124052e54
I K H S A N / <Ikhsancandra721@gmail.com>
2026-09-15 19:43:07 +0700

    Add ASCII art display in ktm.cpp

---
+++ P2/ktm.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main()
+{
+    cout<<"   /\\"<<endl; 
+    cout<<"  /  \\"<<endl;
+    cout<<" /    \\"<<endl;
+    cout<<"|------|"<<endl;
+    cout<<"|  []  |"<<endl;
+    cout<<"|      |"<<endl;
+    cout<<"|______|\n"<<endl;
+    
+    cout<<"   *   "<<endl;
+    cout<<"  ***  "<<endl;
+    cout<<" ***** "<<endl;
+    cout<<"*******"<<endl;
+    cout<<"   |   "<<endl;
+    cout<<"   |   \n"<<endl;
+    
+    cout<<"  /\\_/\\"<<endl;
+    cout<<" ( o.o )"<<endl;
+    cout<<"  > ^ <"<<endl;
+    cout<<" /|   |\\"<<endl;
+    cout<<"(_|   |_)"<<endl;
+return 0;
+}
