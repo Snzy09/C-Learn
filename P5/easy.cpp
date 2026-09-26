af6e385f856b5b96b4b78107b759f252c3a9592f
I K H S A N / <Ikhsancandra721@gmail.com>
2026-09-27 01:27:10 +0700

    Create easy.cpp

---
+++ P5/easy.cpp
@@ -0,0 +1,65 @@
+// Nama : M Ikhsan Candra Putra
+// NIM  : 126140138
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukkan n: ";
+    cin >> n;
+
+    cout << "Output: ";
+    for (int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// Nama : M Ikhsan Candra Putra
+// NIM  : 126140138
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukkan n: ";
+    cin >> n;
+
+    cout << "Output: ";
+    for (int i = n; i >= 1; i--) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// Nama : M Ikhsan Candra Putra
+// NIM  : 126140138
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukkan n: ";
+    cin >> n;
+
+    cout << "Output: ";
+    for (int i = 1; i <= n; i += 2) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
